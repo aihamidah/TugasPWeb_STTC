5fbb92227fa2b01c45a306265661ed0ec79b09f8
RizkiRezaAlGifari <reza10222104@sttcipasung.ac.id>
2025-01-13 22:16:00 +0700

    membuat migration untuk tabel dosen

---
+++ database/migrations/2025_01_13_143932_create_dosens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dosens', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('nidn')->unique();
+            $table->string('program_studi');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dosens');
+    }
+};
