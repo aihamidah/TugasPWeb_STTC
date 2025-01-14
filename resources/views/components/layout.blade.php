3f37ea6aa627d0d84ef29a0891eab8f6d88c141b
Shofi Sabilatus S <shofi10222145@sttcipasung.ac.id>
2025-01-14 22:46:49 +0700

    Tambah tampilan jurusan if
    belum beres
    
    Co-Authored-By: Adzril Fauzi <155979923+AdzrilSchools@users.noreply.github.com>

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,6 +16,16 @@
         <link rel="stylesheet" href="css/Kontak.css" type="text/css" media="all" />
 
         <!-- font awesome -->
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+        <link
+              href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+              rel="stylesheet"
+              integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+              crossorigin="anonymous"
+            />
+
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
@@ -66,9 +76,18 @@
           crossorigin="anonymous"
           referrerpolicy="no-referrer"
         ></script>
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+          ></script>
 
-   
-
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+            integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+            crossorigin="anonymous"
+          ></script>
+      
       </head>
 <body>
 
--- resources/views/informatika.blade.php
+++ resources/views/informatika.blade.php
@@ -1,249 +1,129 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=7" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
-
-    <title>Informatika</title>
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="shortcut icon" href="images/sttc.png" type="image/x-icon" />
-
-    <!-- font awesome -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-      integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"
-      integrity="sha512-RXf+QSDCUQs5uwRKaDoXt55jygZZm2V++WUZduaU/Ui/9EGp3f/2KZVahFZBKGH0s774sd3HmrhUy+SgOFQLVQ=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    ></script>
-
-    <!-- boot -->
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-      integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-      crossorigin="anonymous"
-    />
-
-    <script
-      src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-      crossorigin="anonymous"
-    ></script>
-
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
-      crossorigin="anonymous"
-    ></script>
-
-    <!-- lightslider -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/css/lightslider.css"
-      integrity="sha512-+1GzNJIJQ0SwHimHEEDQ0jbyQuglxEdmQmKsu8KI7QkMPAnyDrL9TAnVyLPEttcTxlnLVzaQgxv2FpLCLtli0A=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/js/lightslider.min.js"
-      integrity="sha512-Gfrxsz93rxFuB7KSYlln3wFqBaXUc1jtt3dGCp+2jTb563qYvnUBM/GP2ZUtRC27STN/zUamFtVFAIsRFoT6/w=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    ></script>
-
-  </head>
-
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light navbarKu fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">
-          <img src="images/logosttc.png" alt="STT Cipasung" />
-          <h1>STT CIPASUNG<br />High Education for All</h1>
-        </a>
-
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto ml-auto">
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto ml-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="{{ url('/') }}">Home</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="{{ url('/profile') }}">Profil</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="{{ url('/fasilitas') }}">Fasilitas</a>
-                        </li>
-                    </ul>
-                </div>
-
-                    <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" id="dropdownMenuLink" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        More
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset">
-                        <a class="dropdown-item" href="{{ url('/ekstrakulikuler') }}">Unit Kegiatan Mahasiswa</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#contact">Contact Us</a>
-                    </div>
-                </li>
-            </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control sm-2" type="search" placeholder="Cari" aria-label="Search"/>
-
-            <button class="btn btn-success sm-0" type="submit">
-              <i class="fas fa-search"></i>
-            </button>
-          </form>
+<x-layout>
+      <br><br><br>
+      <navbar-if>
+        <div class="d-flex flex-column align-items-center text-center w-100">
+            <h2 class="m-0">INFORMATIKA</h2>
+            <p>Sekolah Tinggi Teknologi Cipasung</p>
         </div>
-      </div>
-    </nav>
+        <ul class="d-flex justify-content-center list-unstyled mt-2 mx-0 mb-0">
+            <li class="mx-3">
+                <a href="#about" class="text-white text-decoration-none">Tentang</a>
+            </li>
+            <li class="mx-3">
+                <a href="#programs" class="text-white text-decoration-none">Himpunan</a>
+            </li>
+            <li class="mx-3">
+                <a href="#news" class="text-white text-decoration-none">Dokumentasi</a>
+            </li>
+        </ul>
+    </navbar-if>
+    
 
-    < class="container titleArtikel">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="ukm/images/di1.jpg" class="d-block w-100" alt="Slide 1">
-              </div>
-              {{-- <div class="carousel-item">
-                <img src="ukm/images/di2.jpg" class="d-block w-100" alt="Slide 2">
-              </div>
-              <div class="carousel-item">
-                <img src="ukm/images/di3.jpg" class="d-block w-100" alt="Slide 3">
-              </div>
-              <div class="carousel-item">
-                  <img src="ukm/images/di4.jpg" class="d-block w-100" alt="Slide 2">
-                </div>
-                <div class="carousel-item">
-                  <img src="ukm/images/di5.jpg" class="d-block w-100" alt="Slide 3">
-                </div> --}}
-    </div>
-        </div>
-
-
-    <div class="container mt-4">
-      <div class="row">
-        <div class="col-md-12" style="padding: 20px; border: 1px solid #ddd; border-radius: 8px; background-color: #f9f9f9;">
-          <p style="text-align: justify; color: #333; font-size: 16px; line-height: 1.6;">
-            Jurusan Informatika di Sekolah Tinggi Teknologi (STT) Cipasung dirancang
-                untuk mencetak generasi profesional yang kompeten dalam bidang teknologi informasi
-                dan komunikasi. Dengan kurikulum berbasis teknologi terkini, jurusan ini fokus
-                pada pengembangan keahlian di bidang pemrograman, rekayasa perangkat lunak,
-                analisis data, jaringan komputer, dan pengembangan aplikasi berbasis web serta mobile.
-
-                Kami mengedepankan pendekatan praktis melalui laboratorium berteknologi tinggi,
-                proyek kolaboratif, dan bimbingan dari dosen berpengalaman. Selain itu, jurusan
-                Informatika STT Cipasung juga mendukung pengembangan soft skills seperti berpikir
-                kritis, kreativitas, dan manajemen proyek untuk mempersiapkan lulusan yang siap bersaing
-                di era Revolusi Industri 4.0.
-
-                Bergabunglah dengan kami untuk menjadi inovator teknologi masa depan!
-
-          </p>
-        </div>
-      </div>
-    </div>
-
-@include('components.footer')
-
-    {{-- <div class="footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <h4 class="my-4 text-white text-center">Contact Us</h4>
-
-            <div class="footerContactUs">
-              <div class="perFooterContactUs">
-                <i class="fas text-white fa-envelope"></i>
-                <p class="text-white">admin@sttcipasung.ac.id</p>
-              </div>
-
-              <div class="perFooterContactUs">
-                <i class="fas text-white fa-phone-alt"></i>
-                <p class="text-white">08111121212</p>
-              </div>
-
-              <div class="perFooterContactUs">
-                <i class="fas text-white fa-road"></i>
-                <p class="text-white">
-                  Jl. Cisinga No.KM1, Cilampunghilir, Kec. Padakembang,
-                  Kabupaten Tasikmalaya, Jawa Barat 46466
-                </p>
-              </div>
+    <!-- Hero Section -->
+    <main class="container-lg m-auto p-20 bg-white mb-4">
+      <!-- About Section -->
+      <section id="about" class="mb-4">
+        <div>
+          <div
+            class="row align-items-center justify-content-center text-center"
+          >
+            <div class="col-lg-6">
+              <h2 class="display-4">Tentang Jurusan</h2>
+              <p class="lead">
+                lorem QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW
+                <br />
+                dusgghshsjkdsfsdfddhfdshihsihsi
+              </p>
+            </div>
+            <div class="col-lg-6">
+              <img
+                src="img/"
+                alt="Informatika"
+                class="img-fluid"
+              />
+            </div>
+            <div class="my-4">
+              <button
+                type="button"
+                class="btn btn-outline-primary"
+                onclick="document.getElementById('programs').scrollIntoView({behavior: 'smooth'});"
+              >
+                Pelajari Lebih Lanjut
+              </button>
             </div>
           </div>
+        </div>
+      </section>
 
-          <div class="col-md-4">
-            <h4 class="my-4 text-center text-white">About</h4>
-            <div class="footerAbout">
-              <a href="" class="text-white">Fasilitas</a>
-              <a href="" class="text-white">Galeri</a>
-              <a href="" class="text-white">Contact Us</a>
-              <a href="" class="text-white">Ekstrakulikuler</a>
-            </div>
-          </div>
-
-          <div class="col-md-4 text-center">
-            <h4 class="my-4 text-white">Newsletter</h4>
-            <form>
-              <div class="form-group">
-                <input
-                  class="form-control"
-                  type="search"
-                  placeholder="Example@gmail.com"
-                  aria-label="Search"
+      <!-- Programs Section -->
+      <section id="programs" class="mb-4">
+        <div class="container">
+          <h2 class="text-center mb-4">Himpunan</h2>
+          <div class="row justify-content-center">
+            <!-- Teknik Informatika -->
+            <div class="col-md-4 mb-4">
+              <div class="card h-100">
+                <img
+                  src="assets/img/teknik-informatika.jpg"
+                  class="card-img-top"
+                  alt="Teknik Informatika"
                 />
-                <button class="btn btn-success btn-newsletter" type="submit">
-                  Kirim
-                </button>
+                <div class="card-body">
+                  <h5 class="card-title">Teknik Informatika</h5>
+                  <p class="card-text">
+                    Program yang fokus pada pengembangan perangkat lunak,
+                    pemrograman, kecerdasan buatan, dan teknologi jaringan untuk
+                    mendukung inovasi di era digital.
+                  </p>
+                  <a href="#" class="btn btn-primary">Pelajari Lebih Lanjut</a>
+                </div>
               </div>
-            </form>
+            </div>
+            <!-- Sistem Informasi -->
+            <div class="col-md-4 mb-4">
+              <div class="card h-100">
+                <img
+                  src="assets/img/sistem-informasi.jpg"
+                  class="card-img-top"
+                  alt="Sistem Informasi"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Sistem Informasi</h5>
+                  <p class="card-text">
+                    Menggabungkan teknologi informasi dengan manajemen bisnis
+                    untuk menciptakan solusi berbasis sistem yang efisien dan
+                    efektif dalam pengelolaan data.
+                  </p>
+                  <a href="#" class="btn btn-primary">Pelajari Lebih Lanjut</a>
+                </div>
+              </div>
+            </div>
+            <!-- Manajemen -->
+            <div class="col-md-4 mb-4">
+              <div class="card h-100">
+                <img
+                  src="assets/img/manajemen.jpg"
+                  class="card-img-top"
+                  alt="Manajemen"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Manajemen</h5>
+                  <p class="card-text">
+                    Membekali mahasiswa dengan kemampuan kepemimpinan, strategi
+                    bisnis, dan pengelolaan sumber daya untuk menghadapi
+                    tantangan dunia usaha modern.
+                  </p>
+                  <a href="#" class="btn btn-primary">Pelajari Lebih Lanjut</a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
 
-    <div class="footerCopyright">
-      <p class="text-white">
-        <i class="far fa-copyright"></i>2024 By :
-        <span>ARKOM Group</span>.&nbsp; Inspired by
-        <a href="https://sttcipasung.ac.id/">STT Cipasung</a>
-      </p>
-    </div>
+      
+      
+    </main>
 
-    <script src="js/script.js"></script>
-  </body>
-</html> --}}
+
+  </x-layout>
