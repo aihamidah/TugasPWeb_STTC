dbcb80eaa7aa638d841e1adc8a21083ec9aba478
Adzril Fauzi <adzril10222093@sttcipasung.ac.id>
2025-01-16 19:52:39 +0700

    edit css

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,8 +5,8 @@
           <h1>STT CIPASUNG<br />High Education for All</h1>
         </a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" 
-        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
         aria-expanded="false" aria-label="Toggle navigation">
 
         <span class="navbar-toggler-icon"></span>
