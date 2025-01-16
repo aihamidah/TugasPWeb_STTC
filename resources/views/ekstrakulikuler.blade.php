35dc915d5e94c4f434d9aaa5a4008bf1f2854c67
Adzril Fauzi <adzril10222093@sttcipasung.ac.id>
2025-01-16 19:09:51 +0700

    Perbaiki tampilan UKM
    nambah tema hijau jeung edit pading/spasi/margin pada title

--- resources/views/ekstrakulikuler.blade.php
+++ resources/views/ekstrakulikuler.blade.php
@@ -1,67 +1,54 @@
 <x-layout>
-          
-
-    <div class="title">
+    <div class="title" style="padding: 1px;">
         <h1 class="text-center">UNIT KEGIATAN MAHASISWA</h1>
         <p class="text-center">
-          Beberapa Unit Kegiatan Mahasiswa Yang Ada di STT Cipasung
+            Beberapa Unit Kegiatan Mahasiswa Yang Ada di STT Cipasung
         </p>
-      </div>
-
-
-      <div class="container ekstrakulikuler">
-        {{-- <a href="ukm/kelapa.html" class="perEkstrakulikuler"> --}}
-        <a href ="{{url('/kelapa') }}" class="perEkstrakulikuler">
-        {{-- <a class="nav-link" href="{{ url('/kelapa') }}"></a> --}}
-          <img src="images/ukm/kelapa.png" alt="KELAPA" />
-          <p>KELAPA</p>
+    <div class="container ekstrakulikuler" style="background-color: #00cc00; padding: 0px; border-radius: 50px;">
+        <a href="{{ url('/kelapa') }}" class="perEkstrakulikuler">
+            <img src="images/ukm/kelapa.png" alt="KELAPA" />
+            <p>KELAPA</p>
         </a>
 
-        <a href ="{{url('/kerohanian') }}" class="perEkstrakulikuler">
-          <img src="images/ukm/kerohanian.png" alt="Kerohanian" />
-          <p>Kerohanian</p>
+        <a href="{{ url('/kerohanian') }}" class="perEkstrakulikuler">
+            <img src="images/ukm/kerohanian.png" alt="Kerohanian" />
+            <p>Kerohanian</p>
         </a>
 
-        <a href ="{{url('/olahraga') }}" class="perEkstrakulikuler">
-          <img src="images/ukm/olahraga.png" alt="Olahraga" />
-          <p>Olahraga</p>
+        <a href="{{ url('/olahraga') }}" class="perEkstrakulikuler">
+            <img src="images/ukm/olahraga.png" alt="Olahraga" />
+            <p>Olahraga</p>
         </a>
 
-        <a href ="{{url('/silat') }}" class="perEkstrakulikuler">
-          <img src="images/ukm/silat.png" alt="silat" />
-          <p>Pencak Silat</p>
+        <a href="{{ url('/silat') }}" class="perEkstrakulikuler">
+            <img src="images/ukm/silat.png" alt="silat" />
+            <p>Pencak Silat</p>
         </a>
 
-        <a href ="{{url('/sanggar') }}" class="perEkstrakulikuler">
-          <img src="images/ukm/sanggar.png" alt="Sanggar" />
-          <p>Sanggar</p>
+        <a href="{{ url('/sanggar') }}" class="perEkstrakulikuler">
+            <img src="images/ukm/sanggar.png" alt="Sanggar" />
+            <p>Sanggar</p>
         </a>
 
-        <a href ="{{url('/dignity') }}" class="perEkstrakulikuler">
-          <img src="images/ukm/dignity.png" alt="Dignity" />
-          <p>Dignity</p>
+        <a href="{{ url('/dignity') }}" class="perEkstrakulikuler">
+            <img src="images/ukm/dignity.png" alt="Dignity" />
+            <p>Dignity</p>
         </a>
 
-        <a href ="{{url('/proclub') }}" class="perEkstrakulikuler">
-          <img src="images/ukm/proclub.png" alt="ProClub" />
-          <p>ProClub</p>
+        <a href="{{ url('/proclub') }}" class="perEkstrakulikuler">
+            <img src="images/ukm/proclub.png" alt="ProClub" />
+            <p>ProClub</p>
         </a>
 
-        <a href ="{{url('/rilis') }}" class="perEkstrakulikuler">
-          <img src="images/ukm/rilis.png" alt="Rilis" />
-          <p>Rilis</p>
+        <a href="{{ url('/rilis') }}" class="perEkstrakulikuler">
+            <img src="images/ukm/rilis.png" alt="Rilis" />
+            <p>Rilis</p>
         </a>
 
-        <a href ="{{url('/kdd') }}" class="perEkstrakulikuler">
-          <img src="images/ukm/kdd.png" alt="KDD" />
-          <p>KDD</p>
+        <a href="{{ url('/kdd') }}" class="perEkstrakulikuler">
+            <img src="images/ukm/kdd.png" alt="KDD" />
+            <p>KDD</p>
         </a>
-      </div>
-
     </div>
-
-    <br>
-    <br>
-    <br>
-
+    </div>
 </x-layout>
